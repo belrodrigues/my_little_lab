4fc7fe80a0de9f43b0f816f5e11e1543c6b70231
belrodrigues <belrodrigues@gmail.com>
2011-06-16 20:59:02 -0300

    Removing landing page controller

--- app/controllers/landing_page_controller.rb
+++
@@ -1,6 +0,0 @@
-class LandingPageController < ApplicationController
-  def index
-    @person = Person.new
-    render :action => "people/new"
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ MyLittleLab::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "landing_page#index"
+  #root :to => "landing_page#index"
 
   # See how all your routes lay out with "rake routes"
 
